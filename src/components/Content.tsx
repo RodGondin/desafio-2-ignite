934701d1c83cb42db297c177af3869f10a1299d0
Rodrigo Gondin <rodrigo.gevaerd@compasso.com.br>
2021-09-24 19:39:09 -0300

    desafio-2-Ignite

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,38 @@
-export function Content() {
+import { useEffect } from 'react';
+import { api } from '.././services/api';
+import { MovieCard } from './MovieCard';
+import { Header } from './Header';
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  movies: MovieProps[];
+  selectedGenre: {
+    title: string;
+  };
+}
+
+
+export function Content({movies, selectedGenre}: ContentProps) {
   // Complete aqui
+  return (
+      <div className="container">
+      <Header selectedGenre={selectedGenre.title}/>
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
